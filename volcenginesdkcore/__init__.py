bb8f2e3f6e368f7a78c8ab552d25add6a6c8cc35
zhangpeihua <zhangpeihua@bytedance.com>
2022-11-15 19:03:00 +0800

    feat: update core

--- volcenginesdkcore/__init__.py
+++ volcenginesdkcore/__init__.py
@@ -1,5 +1,4 @@
 # coding:utf-8
 
 from volcenginesdkcore.configuration import Configuration
-from volcenginesdkcore.credentials import Credentials
 from volcenginesdkcore.api_client import ApiClient
