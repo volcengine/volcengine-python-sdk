6d334466fe5e2ef78ec7e26bbb65c7e1872cda4b
zhangpeihua <zhangpeihua@bytedance.com>
2023-07-13 09:09:14 +0800

    feat: update vpc api

--- volcenginesdkvpc/models/describe_bandwidth_packages_request.py
+++ volcenginesdkvpc/models/describe_bandwidth_packages_request.py
@@ -36,7 +36,10 @@ class DescribeBandwidthPackagesRequest(object):
         'bandwidth_package_ids': 'list[str]',
         'bandwidth_package_name': 'str',
         'isp': 'str',
+        'page_number': 'int',
+        'page_size': 'int',
         'project_name': 'str',
+        'protocol': 'str',
         'security_protection_enabled': 'bool',
         'tag_filters': 'list[TagFilterForDescribeBandwidthPackagesInput]'
     }
@@ -45,12 +48,15 @@ class DescribeBandwidthPackagesRequest(object):
         'bandwidth_package_ids': 'BandwidthPackageIds',
         'bandwidth_package_name': 'BandwidthPackageName',
         'isp': 'ISP',
+        'page_number': 'PageNumber',
+        'page_size': 'PageSize',
         'project_name': 'ProjectName',
+        'protocol': 'Protocol',
         'security_protection_enabled': 'SecurityProtectionEnabled',
         'tag_filters': 'TagFilters'
     }
 
-    def __init__(self, bandwidth_package_ids=None, bandwidth_package_name=None, isp=None, project_name=None, security_protection_enabled=None, tag_filters=None, _configuration=None):  # noqa: E501
+    def __init__(self, bandwidth_package_ids=None, bandwidth_package_name=None, isp=None, page_number=None, page_size=None, project_name=None, protocol=None, security_protection_enabled=None, tag_filters=None, _configuration=None):  # noqa: E501
         """DescribeBandwidthPackagesRequest - a model defined in Swagger"""  # noqa: E501
         if _configuration is None:
             _configuration = Configuration()
@@ -59,7 +65,10 @@ class DescribeBandwidthPackagesRequest(object):
         self._bandwidth_package_ids = None
         self._bandwidth_package_name = None
         self._isp = None
+        self._page_number = None
+        self._page_size = None
         self._project_name = None
+        self._protocol = None
         self._security_protection_enabled = None
         self._tag_filters = None
         self.discriminator = None
@@ -70,8 +79,14 @@ class DescribeBandwidthPackagesRequest(object):
             self.bandwidth_package_name = bandwidth_package_name
         if isp is not None:
             self.isp = isp
+        if page_number is not None:
+            self.page_number = page_number
+        if page_size is not None:
+            self.page_size = page_size
         if project_name is not None:
             self.project_name = project_name
+        if protocol is not None:
+            self.protocol = protocol
         if security_protection_enabled is not None:
             self.security_protection_enabled = security_protection_enabled
         if tag_filters is not None:
@@ -148,6 +163,51 @@ class DescribeBandwidthPackagesRequest(object):
         self._isp = isp
 
     @property
+    def page_number(self):
+        """Gets the page_number of this DescribeBandwidthPackagesRequest.  # noqa: E501
+
+
+        :return: The page_number of this DescribeBandwidthPackagesRequest.  # noqa: E501
+        :rtype: int
+        """
+        return self._page_number
+
+    @page_number.setter
+    def page_number(self, page_number):
+        """Sets the page_number of this DescribeBandwidthPackagesRequest.
+
+
+        :param page_number: The page_number of this DescribeBandwidthPackagesRequest.  # noqa: E501
+        :type: int
+        """
+
+        self._page_number = page_number
+
+    @property
+    def page_size(self):
+        """Gets the page_size of this DescribeBandwidthPackagesRequest.  # noqa: E501
+
+
+        :return: The page_size of this DescribeBandwidthPackagesRequest.  # noqa: E501
+        :rtype: int
+        """
+        return self._page_size
+
+    @page_size.setter
+    def page_size(self, page_size):
+        """Sets the page_size of this DescribeBandwidthPackagesRequest.
+
+
+        :param page_size: The page_size of this DescribeBandwidthPackagesRequest.  # noqa: E501
+        :type: int
+        """
+        if (self._configuration.client_side_validation and
+                page_size is not None and page_size > 100):  # noqa: E501
+            raise ValueError("Invalid value for `page_size`, must be a value less than or equal to `100`")  # noqa: E501
+
+        self._page_size = page_size
+
+    @property
     def project_name(self):
         """Gets the project_name of this DescribeBandwidthPackagesRequest.  # noqa: E501
 
@@ -167,6 +227,34 @@ class DescribeBandwidthPackagesRequest(object):
         """
 
         self._project_name = project_name
+
+    @property
+    def protocol(self):
+        """Gets the protocol of this DescribeBandwidthPackagesRequest.  # noqa: E501
+
+
+        :return: The protocol of this DescribeBandwidthPackagesRequest.  # noqa: E501
+        :rtype: str
+        """
+        return self._protocol
+
+    @protocol.setter
+    def protocol(self, protocol):
+        """Sets the protocol of this DescribeBandwidthPackagesRequest.
+
+
+        :param protocol: The protocol of this DescribeBandwidthPackagesRequest.  # noqa: E501
+        :type: str
+        """
+        allowed_values = ["IPv4", "IPv6"]  # noqa: E501
+        if (self._configuration.client_side_validation and
+                protocol not in allowed_values):
+            raise ValueError(
+                "Invalid value for `protocol` ({0}), must be one of {1}"  # noqa: E501
+                .format(protocol, allowed_values)
+            )
+
+        self._protocol = protocol
 
     @property
     def security_protection_enabled(self):
