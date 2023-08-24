cb6e53ad64b405a8cf92f60ab2dcf4b6d6fd3162
zhangpeihua <zhangpeihua@bytedance.com>
2023-08-24 18:09:31 +0800

    feat: update sdk version

--- volcenginesdkcore/configuration.py
+++ volcenginesdkcore/configuration.py
@@ -221,5 +221,5 @@ class Configuration(six.with_metaclass(TypeWithDefault, object)):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 0.1.0\n"\
-               "SDK Package Version: 1.0.37".\
+               "SDK Package Version: 1.0.38".\
                format(env=sys.platform, pyversion=sys.version)
