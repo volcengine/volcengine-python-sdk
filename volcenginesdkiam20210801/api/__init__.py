7ede6b76a61a76f4fe7eb39b20446ad4085e5274
zhangpeihua <zhangpeihua@bytedance.com>
2023-08-24 11:51:10 +0800

    feat: update iam

--- volcenginesdkiam20210801/api/__init__.py
+++ volcenginesdkiam20210801/api/__init__.py
@@ -3,4 +3,4 @@ from __future__ import absolute_import
 # flake8: noqa
 
 # import apis into api package
-from volcenginesdkiam.api.iam_api import IAMApi
+from volcenginesdkiam20210801.api.iam_api import IAMApi
