7ee54eff94745f155a6c8642ed78e83299064333
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2023-09-28 10:55:20 +0800

    feat: auto generate for xuyaming-clb-20230928

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "volcengine-python-sdk"
-VERSION = "1.0.44"
+VERSION = "1.0.45"
 # To install the library, run the following
 #
 # python setup.py install
