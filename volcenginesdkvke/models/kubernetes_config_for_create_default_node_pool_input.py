f5903fd15b8fe90322e452961b0410d087b1ea98
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2024-01-17 10:16:46 +0800

    feat: auto generate for vke-Python-2022-05-12-online-300-2024_01_17_10_16_27

--- volcenginesdkvke/models/kubernetes_config_for_create_default_node_pool_input.py
+++ volcenginesdkvke/models/kubernetes_config_for_create_default_node_pool_input.py
@@ -35,16 +35,18 @@ class KubernetesConfigForCreateDefaultNodePoolInput(object):
     swagger_types = {
         'cordon': 'bool',
         'labels': 'list[LabelForCreateDefaultNodePoolInput]',
+        'name_prefix': 'str',
         'taints': 'list[TaintForCreateDefaultNodePoolInput]'
     }
 
     attribute_map = {
         'cordon': 'Cordon',
         'labels': 'Labels',
+        'name_prefix': 'NamePrefix',
         'taints': 'Taints'
     }
 
-    def __init__(self, cordon=None, labels=None, taints=None, _configuration=None):  # noqa: E501
+    def __init__(self, cordon=None, labels=None, name_prefix=None, taints=None, _configuration=None):  # noqa: E501
         """KubernetesConfigForCreateDefaultNodePoolInput - a model defined in Swagger"""  # noqa: E501
         if _configuration is None:
             _configuration = Configuration()
@@ -52,6 +54,7 @@ class KubernetesConfigForCreateDefaultNodePoolInput(object):
 
         self._cordon = None
         self._labels = None
+        self._name_prefix = None
         self._taints = None
         self.discriminator = None
 
@@ -59,6 +62,8 @@ class KubernetesConfigForCreateDefaultNodePoolInput(object):
             self.cordon = cordon
         if labels is not None:
             self.labels = labels
+        if name_prefix is not None:
+            self.name_prefix = name_prefix
         if taints is not None:
             self.taints = taints
 
@@ -103,6 +108,27 @@ class KubernetesConfigForCreateDefaultNodePoolInput(object):
         """
 
         self._labels = labels
+
+    @property
+    def name_prefix(self):
+        """Gets the name_prefix of this KubernetesConfigForCreateDefaultNodePoolInput.  # noqa: E501
+
+
+        :return: The name_prefix of this KubernetesConfigForCreateDefaultNodePoolInput.  # noqa: E501
+        :rtype: str
+        """
+        return self._name_prefix
+
+    @name_prefix.setter
+    def name_prefix(self, name_prefix):
+        """Sets the name_prefix of this KubernetesConfigForCreateDefaultNodePoolInput.
+
+
+        :param name_prefix: The name_prefix of this KubernetesConfigForCreateDefaultNodePoolInput.  # noqa: E501
+        :type: str
+        """
+
+        self._name_prefix = name_prefix
 
     @property
     def taints(self):
