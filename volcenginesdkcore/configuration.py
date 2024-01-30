481d16fc624a66e06f660c5bf718291d46674e21
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2024-01-30 19:40:22 +0800

    feat: auto generate for billing-Python-2022-01-01-online-321-2024_01_30_19_40_07

--- volcenginesdkcore/configuration.py
+++ volcenginesdkcore/configuration.py
@@ -221,5 +221,5 @@ class Configuration(six.with_metaclass(TypeWithDefault, object)):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 0.1.0\n"\
-               "SDK Package Version: 1.0.63".\
+               "SDK Package Version: 1.0.64".\
                format(env=sys.platform, pyversion=sys.version)
