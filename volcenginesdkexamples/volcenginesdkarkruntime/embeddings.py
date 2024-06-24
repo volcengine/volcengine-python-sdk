47ed822acd6c1220abd07d41df35256e9d354548
hexiaochun <hexiaochun@bytedance.com>
2024-06-24 15:37:09 +0800

    fix: fix embedding example

--- volcenginesdkexamples/volcenginesdkarkruntime/embeddings.py
+++ volcenginesdkexamples/volcenginesdkarkruntime/embeddings.py
@@ -5,6 +5,6 @@ client = Ark()
 print("----- embeddings request -----")
 resp = client.embeddings.create(
     model="${YOUR_ENDPOINT_ID}",
-    input="花椰菜又称菜花、花菜，是一种常见的蔬菜。"
+    input=["花椰菜又称菜花、花菜，是一种常见的蔬菜。"]
 )
 print(resp)
