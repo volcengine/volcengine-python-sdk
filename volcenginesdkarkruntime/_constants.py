df8c5b7ab4d1ecafa345aa0f235f212eb4464b8e
hexiaochun <hexiaochun@bytedance.com>
2024-07-01 20:38:04 +0800

    feat: inc default timeout; embeddings add id/create

--- volcenginesdkarkruntime/_constants.py
+++ volcenginesdkarkruntime/_constants.py
@@ -10,7 +10,7 @@ CLIENT_REQUEST_HEADER = "X-Client-Request-Id"
 SERVER_REQUEST_HEADER = "X-Request-Id"
 
 # default timeout is 1 minutes
-DEFAULT_TIMEOUT = httpx.Timeout(timeout=60.0, connect=60.0)
+DEFAULT_TIMEOUT = httpx.Timeout(timeout=600.0, connect=60.0)
 DEFAULT_MAX_RETRIES = 2
 DEFAULT_CONNECTION_LIMITS = httpx.Limits(
     max_connections=1000, max_keepalive_connections=100
