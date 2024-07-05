73a9043017bd236ddb5a23aba7492fb00db101db
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2024-07-05 16:28:55 +0800

    feat: auto generate for mongodb-Python-2022-01-01-online-572-2024_07_05_16_28_37

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "volcengine-python-sdk"
-VERSION = "1.0.91"
+VERSION = "1.0.92"
 # To install the library, run the following
 #
 # python setup.py install
