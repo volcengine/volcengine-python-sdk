743febe612ea952e4f36f441103c1d3ba77f37e1
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2024-11-27 17:51:54 +0800

    feat: auto generate for advdefence-Python-2023-03-08-online-806-2024_11_27_17_51_24

---
+++ volcenginesdkadvdefence20230308/api/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from volcenginesdkadvdefence20230308.api.advdefence20230308_api import ADVDEFENCE20230308Api
