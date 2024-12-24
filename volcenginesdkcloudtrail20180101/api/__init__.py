79c97bd73ab1b8ccb67f827f91f761ac9b0d3390
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2024-12-24 16:14:37 +0800

    feat: auto generate for cloud_trail-Python-2018-01-01-online-876-2024_12_24_16_14_02

---
+++ volcenginesdkcloudtrail20180101/api/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from volcenginesdkcloudtrail20180101.api.cloud_trail20180101_api import CLOUDTRAIL20180101Api
