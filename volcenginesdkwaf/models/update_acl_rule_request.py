401a65f3e7f9754d47f840b0eff18f93e6ea2920
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2024-12-31 16:27:12 +0800

    feat: auto generate for waf-Python-2023-12-25-online-904-2024_12_31_16_26_32

--- volcenginesdkwaf/models/update_acl_rule_request.py
+++ volcenginesdkwaf/models/update_acl_rule_request.py
@@ -50,6 +50,7 @@ class UpdateAclRuleRequest(object):
         'ip_location_subregion': 'list[str]',
         'name': 'str',
         'prefix_switch': 'int',
+        'project_name': 'str',
         'url': 'str'
     }
 
@@ -71,10 +72,11 @@ class UpdateAclRuleRequest(object):
         'ip_location_subregion': 'IpLocationSubregion',
         'name': 'Name',
         'prefix_switch': 'PrefixSwitch',
+        'project_name': 'ProjectName',
         'url': 'Url'
     }
 
-    def __init__(self, accurate_group=None, acl_type=None, action=None, advanced=None, description=None, enable=None, host_add_type=None, host_group_id=None, host_list=None, id=None, ip_add_type=None, ip_group_id=None, ip_list=None, ip_location_country=None, ip_location_subregion=None, name=None, prefix_switch=None, url=None, _configuration=None):  # noqa: E501
+    def __init__(self, accurate_group=None, acl_type=None, action=None, advanced=None, description=None, enable=None, host_add_type=None, host_group_id=None, host_list=None, id=None, ip_add_type=None, ip_group_id=None, ip_list=None, ip_location_country=None, ip_location_subregion=None, name=None, prefix_switch=None, project_name=None, url=None, _configuration=None):  # noqa: E501
         """UpdateAclRuleRequest - a model defined in Swagger"""  # noqa: E501
         if _configuration is None:
             _configuration = Configuration()
@@ -97,6 +99,7 @@ class UpdateAclRuleRequest(object):
         self._ip_location_subregion = None
         self._name = None
         self._prefix_switch = None
+        self._project_name = None
         self._url = None
         self.discriminator = None
 
@@ -128,6 +131,8 @@ class UpdateAclRuleRequest(object):
         self.name = name
         if prefix_switch is not None:
             self.prefix_switch = prefix_switch
+        if project_name is not None:
+            self.project_name = project_name
         self.url = url
 
     @property
@@ -498,6 +503,27 @@ class UpdateAclRuleRequest(object):
         """
 
         self._prefix_switch = prefix_switch
+
+    @property
+    def project_name(self):
+        """Gets the project_name of this UpdateAclRuleRequest.  # noqa: E501
+
+
+        :return: The project_name of this UpdateAclRuleRequest.  # noqa: E501
+        :rtype: str
+        """
+        return self._project_name
+
+    @project_name.setter
+    def project_name(self, project_name):
+        """Sets the project_name of this UpdateAclRuleRequest.
+
+
+        :param project_name: The project_name of this UpdateAclRuleRequest.  # noqa: E501
+        :type: str
+        """
+
+        self._project_name = project_name
 
     @property
     def url(self):
