96c64c4fb237f192299f8c3c240a534bafdbddae
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2025-02-19 11:33:22 +0800

    feat: auto generate for vke-Python-2022-05-12-online-978-2025_02_19_11_32_53

--- volcenginesdkvke/api/vke_api.py
+++ volcenginesdkvke/api/vke_api.py
@@ -1100,6 +1100,103 @@ class VKEApi(object):
             _request_timeout=params.get('_request_timeout'),
             collection_formats=collection_formats)
 
+    def exec_container_image_commitment(self, body, **kwargs):  # noqa: E501
+        """exec_container_image_commitment  # noqa: E501
+
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.exec_container_image_commitment(body, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param ExecContainerImageCommitmentRequest body: (required)
+        :return: ExecContainerImageCommitmentResponse
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('async_req'):
+            return self.exec_container_image_commitment_with_http_info(body, **kwargs)  # noqa: E501
+        else:
+            (data) = self.exec_container_image_commitment_with_http_info(body, **kwargs)  # noqa: E501
+            return data
+
+    def exec_container_image_commitment_with_http_info(self, body, **kwargs):  # noqa: E501
+        """exec_container_image_commitment  # noqa: E501
+
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.exec_container_image_commitment_with_http_info(body, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param ExecContainerImageCommitmentRequest body: (required)
+        :return: ExecContainerImageCommitmentResponse
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        all_params = ['body']  # noqa: E501
+        all_params.append('async_req')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        params = locals()
+        for key, val in six.iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method exec_container_image_commitment" % key
+                )
+            params[key] = val
+        del params['kwargs']
+        # verify the required parameter 'body' is set
+        if self.api_client.client_side_validation and ('body' not in params or
+                                                       params['body'] is None):  # noqa: E501
+            raise ValueError("Missing the required parameter `body` when calling `exec_container_image_commitment`")  # noqa: E501
+
+        collection_formats = {}
+
+        path_params = {}
+
+        query_params = []
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+        if 'body' in params:
+            body_params = params['body']
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['application/json'])  # noqa: E501
+
+        # HTTP header `Content-Type`
+        header_params['Content-Type'] = self.api_client.select_header_content_type(  # noqa: E501
+            ['application/json'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = ['volcengineSign']  # noqa: E501
+
+        return self.api_client.call_api(
+            '/ExecContainerImageCommitment/2022-05-12/vke/post/application_json/', 'POST',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='ExecContainerImageCommitmentResponse',  # noqa: E501
+            auth_settings=auth_settings,
+            async_req=params.get('async_req'),
+            _return_http_data_only=params.get('_return_http_data_only'),
+            _preload_content=params.get('_preload_content', True),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=collection_formats)
+
     def forward_kubernetes_api(self, body, **kwargs):  # noqa: E501
         """forward_kubernetes_api  # noqa: E501
 
