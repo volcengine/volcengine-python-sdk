eb48f3b26cc4e7812540f6908d0f4f790697f9d1
xiujie.1 <xiujie.1@bytedance.com>
2025-04-17 19:01:53 +0800

    update version and commitId

--- volcenginesdkcore/configuration.py
+++ volcenginesdkcore/configuration.py
@@ -250,5 +250,5 @@ class Configuration(six.with_metaclass(TypeWithDefault, object)):
                "OS: {env}\n" \
                "Python Version: {pyversion}\n" \
                "Version of the API: 0.1.0\n" \
-               "SDK Package Version: 1.1.3".\
+               "SDK Package Version: 1.1.5".\
             format(env=sys.platform, pyversion=sys.version)
