c901230ada50cf3893060e8ca47df8d05d75a12b
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2025-04-21 16:39:52 +0800

    feat: auto generate for ml_platform-Python-2024-07-01-online-1114-2025_04_21_16_39_19

---
+++ volcenginesdkmlplatform20240701/api/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from volcenginesdkmlplatform20240701.api.ml_platform20240701_api import MLPLATFORM20240701Api
