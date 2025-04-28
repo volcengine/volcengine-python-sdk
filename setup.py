393c72f18aa9a87e87d73d2ee50514ed9f64a71d
lishuxiang <lishuxiang@bytedance.com>
2025-04-28 17:41:49 +0800

    build(version): bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "volcengine-python-sdk"
-VERSION = "1.1.6"
+VERSION = "2.0.1"
 # To install the library, run the following
 #
 # python setup.py install
