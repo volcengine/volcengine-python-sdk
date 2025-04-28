101eb260ae3be1d3612b280b3abceea0c7301527
xiujie.1 <xiujie.1@bytedance.com>
2025-04-28 18:56:24 +0800

    update version and commitId

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "volcengine-python-sdk"
-VERSION = "1.1.5"
+VERSION = "1.1.7"
 # To install the library, run the following
 #
 # python setup.py install
