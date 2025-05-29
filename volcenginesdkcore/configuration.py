c08190344784ba06fc3084618cedebbe8576f295
xiujie.1 <xiujie.1@bytedance.com>
2025-05-29 22:43:30 +0800

    update

--- volcenginesdkcore/configuration.py
+++ volcenginesdkcore/configuration.py
@@ -257,5 +257,5 @@ class Configuration(six.with_metaclass(TypeWithDefault, object)):
                "OS: {env}\n" \
                "Python Version: {pyversion}\n" \
                "Version of the API: 0.1.0\n" \
-               "SDK Package Version: 3.0.6".\
+               "SDK Package Version: 3.0.7".\
             format(env=sys.platform, pyversion=sys.version)
