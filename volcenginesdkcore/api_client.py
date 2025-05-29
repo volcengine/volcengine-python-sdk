66443bc8022ab9e64a81afe67524ac0e410e7808
xiujie.1 <xiujie.1@bytedance.com>
2025-05-29 22:46:57 +0800

    update

--- volcenginesdkcore/api_client.py
+++ volcenginesdkcore/api_client.py
@@ -63,7 +63,7 @@ class ApiClient(object):
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'volcstack-python-sdk/3.0.6'
+        self.user_agent = 'volcstack-python-sdk/3.0.7'
         self.client_side_validation = configuration.client_side_validation
 
         self.interceptor_chain = InterceptorChain()
