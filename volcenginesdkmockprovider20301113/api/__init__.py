d36f116b2050eefd61a26a120e749c053e4b0cbe
lishuxiang <lishuxiang@bytedance.com>
2025-09-09 16:19:07 +0800

    fix

--- volcenginesdkmockprovider20301113/api/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-
-# flake8: noqa
-
-# import apis into api package
-from volcenginesdkmockprovider20301113.api.mock_provider20301113_api import MOCKPROVIDER20301113Api
