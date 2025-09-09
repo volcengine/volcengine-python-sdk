4021ee2d5dcfb1ece33b3c9138ea37dddbfbfc0d
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2025-09-09 15:56:27 +0800

    feat: auto generate for mock_provider-Python-2030-11-13-online-1545-2025_09_09_15_55_57

---
+++ volcenginesdkmockprovider20301113/api/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from volcenginesdkmockprovider20301113.api.mock_provider20301113_api import MOCKPROVIDER20301113Api
