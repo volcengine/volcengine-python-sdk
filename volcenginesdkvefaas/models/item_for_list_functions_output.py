cdc9ad390850d539a637016b0aae6a6367d444b5
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2025-11-19 15:42:51 +0800

    feat: auto generate for vefaas-Python-2024-06-06-online-1848-2025_11_19_15_42_18

--- volcenginesdkvefaas/models/item_for_list_functions_output.py
+++ volcenginesdkvefaas/models/item_for_list_functions_output.py
@@ -41,6 +41,7 @@ class ItemForListFunctionsOutput(object):
         'cpu_strategy': 'str',
         'creation_time': 'str',
         'description': 'str',
+        'enable_apmplus': 'str',
         'envs': 'list[EnvForListFunctionsOutput]',
         'exclusive_mode': 'bool',
         'function_type': 'str',
@@ -77,6 +78,7 @@ class ItemForListFunctionsOutput(object):
         'cpu_strategy': 'CpuStrategy',
         'creation_time': 'CreationTime',
         'description': 'Description',
+        'enable_apmplus': 'EnableApmplus',
         'envs': 'Envs',
         'exclusive_mode': 'ExclusiveMode',
         'function_type': 'FunctionType',
@@ -104,7 +106,7 @@ class ItemForListFunctionsOutput(object):
         'vpc_config': 'VpcConfig'
     }
 
-    def __init__(self, cell=None, code_size=None, code_size_limit=None, command=None, cpu=None, cpu_strategy=None, creation_time=None, description=None, envs=None, exclusive_mode=None, function_type=None, id=None, initializer_sec=None, instance_type=None, last_update_time=None, max_concurrency=None, memory_mb=None, name=None, nas_storage=None, owner=None, port=None, project_name=None, request_timeout=None, role=None, runtime=None, source=None, source_location=None, source_type=None, tags=None, tls_config=None, tos_mount_config=None, triggers_count=None, vpc_config=None, _configuration=None):  # noqa: E501
+    def __init__(self, cell=None, code_size=None, code_size_limit=None, command=None, cpu=None, cpu_strategy=None, creation_time=None, description=None, enable_apmplus=None, envs=None, exclusive_mode=None, function_type=None, id=None, initializer_sec=None, instance_type=None, last_update_time=None, max_concurrency=None, memory_mb=None, name=None, nas_storage=None, owner=None, port=None, project_name=None, request_timeout=None, role=None, runtime=None, source=None, source_location=None, source_type=None, tags=None, tls_config=None, tos_mount_config=None, triggers_count=None, vpc_config=None, _configuration=None):  # noqa: E501
         """ItemForListFunctionsOutput - a model defined in Swagger"""  # noqa: E501
         if _configuration is None:
             _configuration = Configuration()
@@ -118,6 +120,7 @@ class ItemForListFunctionsOutput(object):
         self._cpu_strategy = None
         self._creation_time = None
         self._description = None
+        self._enable_apmplus = None
         self._envs = None
         self._exclusive_mode = None
         self._function_type = None
@@ -161,6 +164,8 @@ class ItemForListFunctionsOutput(object):
             self.creation_time = creation_time
         if description is not None:
             self.description = description
+        if enable_apmplus is not None:
+            self.enable_apmplus = enable_apmplus
         if envs is not None:
             self.envs = envs
         if exclusive_mode is not None:
@@ -379,6 +384,27 @@ class ItemForListFunctionsOutput(object):
         """
 
         self._description = description
+
+    @property
+    def enable_apmplus(self):
+        """Gets the enable_apmplus of this ItemForListFunctionsOutput.  # noqa: E501
+
+
+        :return: The enable_apmplus of this ItemForListFunctionsOutput.  # noqa: E501
+        :rtype: str
+        """
+        return self._enable_apmplus
+
+    @enable_apmplus.setter
+    def enable_apmplus(self, enable_apmplus):
+        """Sets the enable_apmplus of this ItemForListFunctionsOutput.
+
+
+        :param enable_apmplus: The enable_apmplus of this ItemForListFunctionsOutput.  # noqa: E501
+        :type: str
+        """
+
+        self._enable_apmplus = enable_apmplus
 
     @property
     def envs(self):
