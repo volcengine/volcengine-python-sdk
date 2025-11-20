879b31d917aa0eaa0f125ffca5e2dbb15ba7ea8d
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2025-11-20 14:53:09 +0800

    feat: auto generate for vod-Python-2025-01-01-online-1855-2025_11_20_14_52_38

--- volcenginesdkvod20250101/models/task_for_get_execution_output.py
+++ volcenginesdkvod20250101/models/task_for_get_execution_output.py
@@ -45,6 +45,7 @@ class TaskForGetExecutionOutput(object):
         'storyline': 'StorylineForGetExecutionOutput',
         'type': 'str',
         'video_generation': 'VideoGenerationForGetExecutionOutput',
+        'video_matting': 'VideoMattingForGetExecutionOutput',
         'video_summary': 'VideoSummaryForGetExecutionOutput',
         'video_understanding': 'VideoUnderstandingForGetExecutionOutput',
         'vision': 'VisionForGetExecutionOutput'
@@ -63,12 +64,13 @@ class TaskForGetExecutionOutput(object):
         'storyline': 'Storyline',
         'type': 'Type',
         'video_generation': 'VideoGeneration',
+        'video_matting': 'VideoMatting',
         'video_summary': 'VideoSummary',
         'video_understanding': 'VideoUnderstanding',
         'vision': 'Vision'
     }
 
-    def __init__(self, ad_audit=None, asr=None, audio_extract=None, enhance=None, erase=None, file_delete=None, highlight=None, ocr=None, segment=None, storyline=None, type=None, video_generation=None, video_summary=None, video_understanding=None, vision=None, _configuration=None):  # noqa: E501
+    def __init__(self, ad_audit=None, asr=None, audio_extract=None, enhance=None, erase=None, file_delete=None, highlight=None, ocr=None, segment=None, storyline=None, type=None, video_generation=None, video_matting=None, video_summary=None, video_understanding=None, vision=None, _configuration=None):  # noqa: E501
         """TaskForGetExecutionOutput - a model defined in Swagger"""  # noqa: E501
         if _configuration is None:
             _configuration = Configuration()
@@ -86,6 +88,7 @@ class TaskForGetExecutionOutput(object):
         self._storyline = None
         self._type = None
         self._video_generation = None
+        self._video_matting = None
         self._video_summary = None
         self._video_understanding = None
         self._vision = None
@@ -115,6 +118,8 @@ class TaskForGetExecutionOutput(object):
             self.type = type
         if video_generation is not None:
             self.video_generation = video_generation
+        if video_matting is not None:
+            self.video_matting = video_matting
         if video_summary is not None:
             self.video_summary = video_summary
         if video_understanding is not None:
@@ -373,6 +378,27 @@ class TaskForGetExecutionOutput(object):
         """
 
         self._video_generation = video_generation
+
+    @property
+    def video_matting(self):
+        """Gets the video_matting of this TaskForGetExecutionOutput.  # noqa: E501
+
+
+        :return: The video_matting of this TaskForGetExecutionOutput.  # noqa: E501
+        :rtype: VideoMattingForGetExecutionOutput
+        """
+        return self._video_matting
+
+    @video_matting.setter
+    def video_matting(self, video_matting):
+        """Sets the video_matting of this TaskForGetExecutionOutput.
+
+
+        :param video_matting: The video_matting of this TaskForGetExecutionOutput.  # noqa: E501
+        :type: VideoMattingForGetExecutionOutput
+        """
+
+        self._video_matting = video_matting
 
     @property
     def video_summary(self):
