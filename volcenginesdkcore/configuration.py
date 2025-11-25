dc24541ea9f96311c92fbf192413b53d29ebac78
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2025-11-25 14:28:13 +0800

    feat: auto generate for acep-Python-2025-05-01-online-1868-2025_11_25_14_27_37

--- volcenginesdkcore/configuration.py
+++ volcenginesdkcore/configuration.py
@@ -272,7 +272,7 @@ class Configuration(six.with_metaclass(TypeWithDefault, object)):
                "OS: {env}\n" \
                "Python Version: {pyversion}\n" \
                "Version of the API: 0.1.0\n" \
-               "SDK Package Version: 4.0.35".\
+               "SDK Package Version: 4.0.36".\
             format(env=sys.platform, pyversion=sys.version)
 
     @property
