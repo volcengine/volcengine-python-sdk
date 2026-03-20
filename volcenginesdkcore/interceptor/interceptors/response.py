f2c9663e6f3d2dcba35134cfcf9f6e114887aaf2
Liu Zhen <lzhen.dev@outlook.com>
2026-03-20 15:12:10 +0800

    Add JSON parsing to Response class initialization

--- volcenginesdkcore/interceptor/interceptors/response.py
+++ volcenginesdkcore/interceptor/interceptors/response.py
@@ -1,5 +1,12 @@
+import json
 class Response:
     def __init__(self, http_response):
         self.http_response = http_response
         self.result = None
         self.metadata = None
+        self.__post_init__()
+
+    def __post_init__(self):
+        data = json.loads(self.http_response.data)
+        self.result = data.get("Result")
+        self.metadata = data.get("ResponseMetadata")
