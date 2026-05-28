3e5ccf398d128b9998ee78eba78979edca969e28
lishuxiang <lishuxiang@bytedance.com>
2026-05-28 18:07:51 +0800

    fix

--- volcenginesdkresourcecenter20230601/api/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-
-# flake8: noqa
-
-# import apis into api package
-from volcenginesdkresourcecenter20230601.api.resourcecenter20230601_api import RESOURCECENTER20230601Api
