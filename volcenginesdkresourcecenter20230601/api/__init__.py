feb649606e8dbd974e2f2b8c39f4ef693b6cd204
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2026-05-28 17:56:12 +0800

    feat: auto generate for integration_20260528_175542_Python

---
+++ volcenginesdkresourcecenter20230601/api/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from volcenginesdkresourcecenter20230601.api.resourcecenter20230601_api import RESOURCECENTER20230601Api
