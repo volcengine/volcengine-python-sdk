06f715591f1c58672ea19d646d7ede45bb4e50ef
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2026-06-10 20:22:47 +0800

    feat: auto generate for cloudmonitor-Python-2025-12-01-online-2800-2026_06_10_20_22_20

---
+++ volcenginesdkcloudmonitor20251201/api/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from volcenginesdkcloudmonitor20251201.api.cloudmonitor20251201_api import CLOUDMONITOR20251201Api
