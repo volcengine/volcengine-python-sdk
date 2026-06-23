ddea54e5566abe26c4feec52f3828aace7685456
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2026-06-23 19:46:44 +0800

    feat: auto generate for dramart-Python-2026-01-12-online-2840-2026_06_23_19_46_13

--- volcenginesdkcore/endpoint/providers/default_provider.py
+++ volcenginesdkcore/endpoint/providers/default_provider.py
@@ -558,6 +558,12 @@ default_endpoint = {
         global_endpoint='',
         region_endpoint_map={},
     ),
+    'dramart': ServiceEndpointInfo(
+        service='dramart',
+        is_global=True,
+        global_endpoint='',
+        region_endpoint_map={},
+    ),
 }
 
 bootstrap_region = {
